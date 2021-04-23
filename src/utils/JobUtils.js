b3ca9dee0c5399347215ab44dea84d30041b1223
Victor santos <victor_santos1162@hotmail.com>
2021-04-23 13:09:08 -0300

    Projeto freelancer NLW RocketSeat

---
+++ src/utils/JobUtils.js
@@ -0,0 +1,24 @@
+module.exports = {
+    remainingDays(job){
+
+        //calculo de tempo restante
+        const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+    
+            const createdDate = new Date(job.created_at)
+            const dueDay = createdDate.getDate() + Number(remainingDays)
+            const dueDateInMs = createdDate.setDate(dueDay)
+    
+    
+            
+            const timeDiffInMs = dueDateInMs - Date.now()
+            //transformar milli em dias
+    
+            //               ms     s   min   hr 
+            const dayInMs = 1000 * 60 * 60 * 24
+            const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+
+    // restam x dias
+     return dayDiff
+    },
+    calculateBudget: (job, valueHour) => valueHour * job["total-hours"]
+}
